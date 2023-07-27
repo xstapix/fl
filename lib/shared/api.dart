de724d04d94e65808164bd835365b758f18a6392
xstapix <sarafannikov.85@mail.ru>
2023-07-27 19:00:38 +0300

    new api

--- lib/shared/api.dart
+++ lib/shared/api.dart
@@ -1,44 +1,51 @@
 import 'dart:convert';
 import 'dart:async';
+import 'dart:io';
 
 import 'package:dio/dio.dart';
 import 'package:http/http.dart' as http;
 
 var _url = 'https://app.rocketai.ru';
+String _publicToken = 'dX633jpLpxqVY5cuePqI50FrvyOix5HprsILrV6Sq1Jzh9yoCR0nIm3dYw7nmYBXRghDGLMZx5mBjWj1mYBh1260yrZhnrD6Ipq0woAuZNg8B0jTYC30anHntH4U2Y2iFQXlQFLJS3NrYCFmfqkvPfZ3lavzi3QxTfcNcwGgdiNHa8ZuxPhTWBEFtsTOzW8GoRJBPagY3s8wpWb9C7Y5P6CUcDdnDHwkc0qQaUQEfqHqqHh5DEvpmwgk2wFTbr2';
 
-Future sendWork(file) async {
-  List<int> imageBytes = await file.readAsBytes();
+Future sendWork(obj) async {
+  List<int> imageBytes = await obj['file'].readAsBytes();
   String base64Image = base64Encode(imageBytes);
-  print(base64Image);
 
   final response = await http.post(
-    Uri.parse('https://kronadev.ru/flutter.php'),
+    Uri.parse('$_url/api/v1/gen/control-net'),
     body: jsonEncode(
       {
-        "PublicToken": "string",
-        "PicBase64": base64Image,
-        "Prompt": "",
-        "NegPrompt": "",
-        "SDModel": "anything-v4.5.ckpt [fbcf965a62]",
-        "Model": "string",
-        "Module": "string",
-        "A": "string",
-        "B": "string",
-        "ControlWeight": 1,
-        "StartingControlStep": 0,
-        "EndingControlStep": 1,
-        "ControlMode": 2,
-        "denoising_strength": 0.7,
-        "Steps": 25,
-        "cfg_scale": 7,
-        "seed": "-1",
-        "Type": "txt2img"
+        "PublicToken": _publicToken,
+        "SDModel": "deliberate_v2.ckpt [b4391b7978]",
+        "Model": "control_v11p_sd15_lineart",
+        "Module": obj['effect'],
+        "PicBase64": base64Image
       }
-    )
+    ),
+    headers: {HttpHeaders.contentTypeHeader: 'application/json'},
   );
   final data = json.decode(response.body);
 
-  print('data ${data}');
+  return data['PicBase64'][0];
+}
+
+Future txtimg(file) async {
+  List<int> imageBytes = await file.readAsBytes();
+  String base64Image = base64Encode(imageBytes);
+
+  final response = await http.post(
+    Uri.parse('$_url/api/v1/gen/txt2img'),
+    body: jsonEncode(
+      {
+        "PublicToken": _publicToken,
+        "Prompt": '',
+
+      }
+    ),
+    headers: {HttpHeaders.contentTypeHeader: 'application/json'}, 
+  );
+  final data = json.decode(response.body);
 
   return data;
 }
